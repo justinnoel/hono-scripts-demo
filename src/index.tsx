cdb6005ed1bf35bf5b477205f54a8aac3ead6853
Justin Noel <81643826+justinnoel@users.noreply.github.com>
2024-07-29 17:30:33 -0500

    feat: add button & script interaction with `html`

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { Hono } from 'hono'
+import { html, raw } from 'hono/html'
 import { renderer } from './renderer'
 
 const app = new Hono()
@@ -6,7 +7,28 @@ const app = new Hono()
 app.use(renderer)
 
 app.get('/', (c) => {
-  return c.render(<h1>Hello!</h1>)
+  const name = c.req.query('name') || "unknown"
+
+  // This will display the name in the server terminal output, not the browser console.
+  console.log(name)
+
+  return c.render(<>
+    <h1>Hello! This is a test app 🦖</h1>
+
+    <p>
+      <button id='myButton'>Click here</button>
+    </p>
+
+    {html`
+      <script>
+          document.addEventListener('DOMContentLoaded', function() { 
+            document.getElementById('myButton').addEventListener('click', function() {
+              alert("Hi ${raw(name)}!");
+            })
+          })
+      </script>
+    `}
+  </>)
 })
 
 export default app
